b265a4551106408f53e6b222daadb43c109f55b5
bhuvanabharath9795-rgb <bhuvanabharath9795@gmail.com>
2026-04-20 23:23:14 +0530

    fix interceptor

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-
-
 const API = axios.create({
   baseURL: "https://online-vehicle-rental-backend.onrender.com",
   withCredentials: true,
 });
 
-api.interceptors.request.use((config) => {
+// ✅ use same variable name (API)
+API.interceptors.request.use((config) => {
   const userInfo = localStorage.getItem("userInfo");
   if (userInfo) {
     config.headers.Authorization = `Bearer ${JSON.parse(userInfo).token}`;
